37c1874b83ace363efbeb7c603a427882b82a134
hologutbiz <holoakinseloyin@gmail.com>
2026-08-09 20:58:09 +0100

    Change result api

--- scripts/fetchResults.js
+++ scripts/fetchResults.js
@@ -1,6 +1,5 @@
-// const RESULT_API = "https://api.babaijebu.visuallottoboard.com/publicwebgameresults";
-const RESULT_API = "https://visual-lotto-board-results-file.netlify.app/.netlify/functions/get-babaijebu-results"; // Fullback
-// https://visual-lotto-board-results-file.netlify.app/.netlify/functions/get-babaijebu-results
+const RESULT_API = "https://api.babaijebu.visuallottoboard.com/publicwebgameresults";
+// const RESULT_API = "https://visual-lotto-board-results-file.netlify.app/.netlify/functions/get-babaijebu-results"; // Fullback
 
 function normalizeGameName(gameName) {
     const nameMap = {
